27efefca08cd5e119a229a2772ef87edc8af22c4
ReyLeal <reinaldoleal2@gmail.com>
2024-07-29 11:21:10 -0300

    Sync

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ using NHibernate.Driver;
 using System.Reflection;
 using TestesNHibernate.Models;
 using NHibernate;
+using NHibernate.Criterion;
 
 namespace TestesNHibernate
 {
@@ -35,6 +36,19 @@ namespace TestesNHibernate
 
             using (var session = sessionFactory.OpenSession())
             {
+                //QUERYS - Execucao direta de querys
+                //(Nao recomendado, usado apenas para operacoes nao comtempladas pelo core NHibernate)                
+                using (var transaction = session.BeginTransaction())
+                {
+                    session.CreateSQLQuery("DELETE FROM Produto").ExecuteUpdate();
+                    session.CreateSQLQuery("DBCC CHECKIDENT ('Produto', RESEED, 0)").ExecuteUpdate();
+                    session.CreateSQLQuery("DELETE FROM Usuario").ExecuteUpdate();
+                    session.CreateSQLQuery("DBCC CHECKIDENT ('Usuario', RESEED, 0)").ExecuteUpdate();
+                    transaction.Commit();
+                    Console.WriteLine($"Dados limpos e ids resetados!");
+                }
+
+                //CRUD
                 //CREATE
                 using (ITransaction transaction = session.BeginTransaction())
                 {
@@ -42,6 +56,7 @@ namespace TestesNHibernate
                     session.Save(new Produto { nome = "Feijão", preco = 12.90, quantidade = 3 });
                     session.Save(new Produto { nome = "Café", preco = 16.33, quantidade = 4 });
                     session.Save(new Produto { nome = "Leite", preco = 4.99, quantidade = 7 });
+                    session.Save(new Produto { nome = "Macarrão", preco = 6.62, quantidade = 9 });
 
                     session.Save(new Usuario { nome = "Rey", senha = "D2E6A94B8DAB5A9EBA64F71294EAF59D", dataCadastro = DateTime.Now });
                     session.Save(new Usuario { nome = "Ana", senha = "0D5CC8A8A6A9F85F0BAE5EBA92F1959E", dataCadastro = DateTime.Now });
@@ -49,17 +64,21 @@ namespace TestesNHibernate
                     session.Save(new Usuario { nome = "Maria", senha = "A1A09D5BFDFFC3C2B7A0A5914C30D842", dataCadastro = DateTime.Now });
 
                     transaction.Commit();
+                    Console.WriteLine($"Dados cadastrados com sucesso!");
                 }
 
                 //READ ALL
                 Console.WriteLine("Todos produtos");
                 var produtos = session.Query<Produto>().ToList();
+
                 foreach (var p in produtos.ToList())
                 {
                     Console.WriteLine($"{p.id}\t{p.nome}\t{p.preco}\t{p.quantidade}");
                 }
+
                 Console.WriteLine("Todos usuários");
                 var usuarios = session.Query<Usuario>().ToList();
+
                 foreach (var u in usuarios.ToList())
                 {
                     Console.WriteLine($"{u.id}\t{u.nome}\t{u.senha}\t{u.dataCadastro}");
@@ -69,6 +88,7 @@ namespace TestesNHibernate
                 //Busca por id
                 codigoBusca = 3;
                 produto = session.Get<Produto>(codigoBusca);
+
                 if (produto != null)
                 {
                     Console.WriteLine($"Busca por id {codigoBusca}");
@@ -82,6 +102,7 @@ namespace TestesNHibernate
                 //Busca por campo
                 campoBusca = "Arroz";
                 produto = session.Query<Produto>().FirstOrDefault(p => p.nome == campoBusca);
+
                 if (produto != null)
                 {
                     Console.WriteLine($"Busca por campo {campoBusca}");
@@ -90,6 +111,23 @@ namespace TestesNHibernate
                 else
                 {
                     Console.WriteLine($"Produto {campoBusca} não encontrado!");
+                }
+
+                //Busca por criteria (like, and, or)
+                ICriteria criteria = session.CreateCriteria<Produto>();
+                criteria.Add(
+                    Restrictions.Or(
+                        Restrictions.Like("nome", "f%"),
+                        Restrictions.Like("nome", "%ão")
+                    )
+                );
+                IList<Produto> listaDeProdutos = criteria.List<Produto>();
+
+                Console.WriteLine($"Busca por criteria");
+
+                foreach (var p in listaDeProdutos)
+                {
+                    Console.WriteLine($"{p.id}\t{p.nome}\t{p.preco}\t{p.quantidade}");
                 }
 
                 //UPDATE
@@ -119,6 +157,7 @@ namespace TestesNHibernate
                 {
                     codigoBusca = 2;
                     produto = session.Get<Produto>(codigoBusca);
+
                     if (produto != null)
                     {
                         session.Delete(produto);
